99070fd9ffebf9f28906fdab171d7787576c3a1e
dacePetra <dace.petra@gmail.com>
2022-02-05 15:04:27 +0200

    Changes in classes-and-objects/7.php

--- php-basics/classes-and-objects/9.php
+++ php-basics/classes-and-objects/9.php
@@ -28,9 +28,13 @@ echo $bob->showUserNameAndBalance() . PHP_EOL;
 //Add the following method:
 //
 //function show_user_name_and_balance() { }
-//Your method should return a string that contains the account's name and balance separated by a comma and space. For example, if an account object named ben has the name "Benson" and a balance of 17.25, the call of ben.show_user_name_and_balance() should return:
+//Your method should return a string that contains the account's name and balance separated by a comma and space.
+// For example, if an account object named ben has the name "Benson" and a balance of 17.25,
+// the call of ben.show_user_name_and_balance() should return:
 //
 //Benson, $17.25
-//There are some special cases you should handle. If the balance is negative, put the - sign before the dollar sign. Also, always display the cents as a two-digit number. For example, if the same object had a balance of -17.5, your method should return:
+//There are some special cases you should handle. If the balance is negative, put the - sign before the dollar sign.
+// Also, always display the cents as a two-digit number.
+// For example, if the same object had a balance of -17.5, your method should return:
 //
 //Benson, $17.50
